49ac07a76b33c4618f6572759ae8001a90fc0e2f
Michael Druckman <spydrcom@gmail.com>
2022-07-06 14:52:56 -0700

    Fresh Build
    RPC service name changed to CALC from EVAL to avoid conflict with Python
    BAT for application now includes SitStat server ZIP file in class-path
    APP is independent of server, only SERVE command fails w/o ZIP
    FeatureCommands XML changed to support RPC
    Fresh build copies of JavaDocs and classes ZIP
    SitStat server build included as ZIP file

--- calcTools.bat
+++ calcTools.bat
@@ -1,3 +1,3 @@
 c:\
 cd \workspace\deploy
-java -cp myorb.zip;external.zip;gralprop.zip;httpd.zip;splines.zip;sample.zip net.myorb.calctools.config.CalcToolsMDI >session.log
+java -cp myorb.zip;SitStat.zip;external.zip;gralprop.zip;httpd.zip;splines.zip;sample.zip net.myorb.calctools.config.CalcToolsMDI >session.log
