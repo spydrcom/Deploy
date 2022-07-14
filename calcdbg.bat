8ec1971cea959c1bc30d12085843c7879c8febe0
Michael Druckman <spydrcom@gmail.com>
2022-07-14 15:26:46 -0700

    Testbed bug fix
    CFG/testbed now holds old TXT config files

--- calcdbg.bat
+++ calcdbg.bat
@@ -1,3 +1,3 @@
 c:\
 cd \workspace\deploy
-java -cp myorb.zip;external.zip;gralprop.zip;httpd.zip;splines.zip;sample.zip net.myorb.math.expressions.controls.SelectableControl >session.log
+java -cp myorb.zip;external.zip;gralprop.zip;httpd.zip;splines.zip;sample.zip net.myorb.math.expressions.controls.ConfiguredControl >session.log
