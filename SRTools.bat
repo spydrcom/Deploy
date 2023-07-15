9c93d79f985832f6ae1d30adf3ae80cbc7aa48ac
Michael Druckman <spydrcom@gmail.com>
2023-07-15 13:38:24 -0700

    fresh build of Markets application

--- SRTools.bat
+++ SRTools.bat
@@ -1,2 +1,2 @@
 
-java -cp myorb.zip;external.zip;INT.zip;gralprop.zip;httpd.zip;sample.zip net.myorb.markets.StreamOnly >session.log
+java -cp myorb.zip;external.zip;INT.zip;gralprop.zip;httpd.zip;sample.zip net.myorb.markets.SupportResist >session.log
---
+++ Watch.bat
@@ -0,0 +1,2 @@
+
+java -cp myorb.zip;external.zip;INT.zip;gralprop.zip;httpd.zip;sample.zip net.myorb.markets.WatchList >session.log
